2939a295025628ba933b4e5bae5e5616a8e0e387
Cleber <cleber_batista2010@hotmail.com>
2022-04-04 09:31:03 -0300

    Relembrando Java

---
+++ src/Calculadora.java
@@ -0,0 +1,24 @@
+public class Calculadora {
+
+    double entrada1;
+    String operadorLogico;
+    double entrada2;
+    double resultado;
+
+    public void calcular(){
+        if (operadorLogico.equals("+")) {
+            resultado = entrada1 + entrada2;
+        }else if (operadorLogico.equals("-")){
+            resultado = entrada1 - entrada2;
+        } else if (operadorLogico.equals("*")){
+            resultado = entrada1 * entrada2;
+        } else if (operadorLogico.equals("/")){
+            resultado = entrada1 / entrada2;
+        }else {
+            System.out.println("Operador invalido!!");
+        }
+        System.out.println("O Resultado é: "+resultado);
+    }
+
+
+}
---
+++ src/TestaCalculadora.java
@@ -0,0 +1,14 @@
+public class TestaCalculadora {
+    public static void main(String[] args) {
+        Calculadora calculadoraSoma = new Calculadora();
+        calculadoraSoma.entrada1=15;
+        calculadoraSoma.entrada2=20;
+        calculadoraSoma.operadorLogico="+";
+        calculadoraSoma.calcular();
+
+        //Criar mais objetos subtração - Divisão - Multiplicação
+
+
+
+    }
+}
